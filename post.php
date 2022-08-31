e0bef1e43de5b9101a16cef8dc6fe14f3cd7e04c
knjje <s6303051623161@kmutnb.ac.th>
2022-08-31 12:06:27 +0700

    php

---
+++ post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1 align="center">Webboard JUKJIK</h1>
+    <hr>
+    <div align="center">ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"]; ?></div>
+    <br>
+    <table style="border: 2px solid black; width: 40%" align="center">
+    <tr><td style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
+    <tr><td colspan="2"><textarea name="" id="" cols="100" rows="3"></textarea></td></tr>
+    <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+</table>
+
+<br>
+<center><a href="index.html">กลับไปหน้าหลัก</a></div></center>
+</body>
+</html>
