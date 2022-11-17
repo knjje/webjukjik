5974667158744064a82cdd7dede843b049541543
knjje <s6303051623161@kmutnb.ac.th>
2022-11-17 23:58:50 +0700

    work9

--- newpost.php
+++ newpost.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['id'])){
+if (!isset($_SESSION['id'])) {
     header("location:index.php");
     die();
 }
@@ -12,40 +12,85 @@ if(!isset($_SESSION['id'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>Newpost</title>
 </head>
 
 <body>
-    <h1 align="center">Webboard JUKJIK</h1>
-    <hr>
-    <table>
-        <tr><td>
-        <?php 
-        echo "&nbsp;ผู้ใช้ &nbsp; $_SESSION[username]"; 
+    <div class="container">
+        <h1 align="center">Webboard JUKJIK</h1>
+        <?php
+        include "nav.php";
         ?>
-        </td></tr>
-    <tr>
-            <td>หมวดหมู่ : </td>
-            <td><select name="category">
-                    <option value="general">เรื่องทั่วไป</option>
-                    <option value="study">เรื่องเรียน</option>
-                </select> </td>
-        </tr>
-        <tr>
-            <td>หัวข้อ : </td>
-            <td><input type="text" name="h" size="50"></td>
-        </tr>
-        <tr><td>เนื้อหา : </td>
-        <td><textarea name="" id="" cols="30" rows="10"></textarea></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td ><input type="submit" value="บันทึกข้อความ"></td></tr>
-    </table>
-    
+        <?php
 
-        
+        if (!isset($_SESSION["id"])) {
+            header("Location: index.php");
+        }
+        ?>
+        <section class="col-md-5 mx-auto m-3">
+            <div class="card text-card bg-white border-info">
+                <div class="card-header bg-info text-white">ตั้งกระทู้ใหม่</div>
 
+
+                <div class="card-body">
+                    <form action="newpost_save.php" method="post">
+                        <div class="row mb-3">
+                            <label class="col-lg-3 col-form-label">หมวดหมู่ :</label>
+                            <div class="col-lg-9">
+                                <select name="category" class="form-select">
+                                    <!--    
+                        <option value="general">เรื่องทั่วไป</option>
+                        <option value="study">เรื่องเรียน</option>
+                        -->
+                                    <?php
+                            $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+                            $sql = "SELECT * FROM category";
+                            foreach ($conn->query($sql) as $row) {
+
+                                echo "<option value=" . $row['id'] . ">" . $row['name'] . "</option>";
+                            }
+                            $conn = null;
+                            ?>
+                                </select>
+                            </div>
+                        </div>
+
+
+                        <div class="row mb-3">
+                            <label class="col-lg-3 col-form-label">หัวข้อ :</label>
+                            <div class="col-lg-9">
+                                <input type="text" name="topic" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-lg-3 col-form-label">เนื้อหา :</label>
+                            <div class="col-lg-9">
+                                <textarea class="form-control" name="comment" rows="8" required></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <center>
+                                    <button type="submit" class="btn btn-info btn-sm text-white">
+                                        <i class="bi bi-caret-right-square me-1"></i>บันทึกข้อความ
+                                    </button>
+                                </center>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    </div>
+    </section>
+    </div>
 </body>
 
 </html>
--- register_save.php
+++ register_save.php
@@ -1,27 +1,25 @@
 <?php
-    session_start();
-use LDAP\Result;
+   session_start();
+   $login = $_POST['login'];
+   $passwd = $_POST['pwd'];
+   $name = $_POST['name'];
+   $gender = $_POST['gender'];
+   $email = $_POST['email'];
 
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-    $name = $_POST['name'];
-    $gender = $_POST['gender'];
-    $email = $_POST['email'];
-
-    $conn = new PDO("mysql:host=localhost;dbname=webborad;charset=utf8", "root" , "");
-    $password=sha1($password);
-
-    $sql = "SELECT * FROM user where login='$login'";
+   $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+   $passwd = sha1($passwd);
+    $sql = "SELECT * FROM user where login ='$login'";
     $result=$conn->query($sql);
-    if($result->rowCount()==1){
+    if($result->rowCount()>=1){
         $_SESSION['add_login'] = 'error';
     }else{
-        $sql1="INSERT INTO user (login, password, name, gender, email, role) VALUES ('$login', '$password', '$name', '$gender', '$email', 'm')";
-        $conn->exec($sql1);
+        $sql1 = "INSERT INTO user ( login, password, name, gender, email, role) VALUES
+                ('$login','$passwd','$name','$gender','$email', 'm')";
+
+        $conn -> exec($sql1);
         $_SESSION['add_login'] = 'success';
     }
-
     $conn = null;
-    header('location:register.php');
+    header("Location: register.php");       
     die();
 ?>
